5b0ea79714211444e76bd542cbb386531e0820f9
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2025-10-21 01:13:51 -0700

    Add SSH key generation, firewall rule, and compute instance configuration in Terraform. Introduced outputs for instance details and updated example variables. Added startup script for instance initialization with Docker setup.

--- variables.tf
+++ variables.tf
@@ -14,3 +14,27 @@ variable "zone" {
   type        = string
   default     = "us-central1-a"
 }
+
+variable "machine_type" {
+  description = "GCP machine type"
+  type        = string
+  default     = "e2-standard-4"
+}
+
+variable "disk_size_gb" {
+  description = "Boot disk size in GB"
+  type        = number
+  default     = 50
+}
+
+variable "instance_name" {
+  description = "Name of the compute instance"
+  type        = string
+  default     = "scarab-instance"
+}
+
+variable "ssh_user" {
+  description = "SSH username"
+  type        = string
+  default     = "ubuntu"
+}
